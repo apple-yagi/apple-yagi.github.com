acf001fea4a6a710d02ce826bc242d0b132b584f
apple-yagi <applesryu@gmail.com>
2022-09-18 22:17:53 +0900

    update

---
+++ src/components/Bio/Bio.tsx
@@ -0,0 +1,14 @@
+export const Bio = (): JSX.Element => (
+  <div className="p-5 flex justify-center items-center shadow-2xl rounded-3xl">
+    <div>
+      <img
+        className="w-128 h-128 object-contain rounded-full"
+        src="/avatar.png"
+      />
+    </div>
+    <h1 className="text-primary text-2xl font-bold">Ryuya Yanagi</h1>
+    <ul className="flex">
+      <li></li>
+    </ul>
+  </div>
+);
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#36395a",
+        secondary: "#484c7a",
+      },
+    },
   },
   plugins: [],
 };
